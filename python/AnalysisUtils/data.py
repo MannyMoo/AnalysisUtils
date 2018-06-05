7e448026aaba19b1c3bbc27415bda87e57c728f8
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-06-05 12:42:03 +0100

    Rework data stuff into a class.

--- python/AnalysisUtils/data.py
+++ python/AnalysisUtils/data.py
@@ -5,47 +5,57 @@ from AnalysisUtils.makeroodataset import make_roodataset
 from AnalysisUtils.treeutils import make_chain
 from array import array
 
-def get_data(datapaths, name) :
-    '''Get the dataset of the given name.'''
-    try :
-        return make_chain(*datapaths[name])
-    except KeyError :
-        raise ValueError('Unknown data type: ' + repr(name))
+class DataLibrary(object) :
 
-def dataset_file_name(datapaths, dataname) :
-    '''Get the name of the file containing the RooDataset corresponding to the given
-    dataset name.'''
-    return os.path.join(os.path.dirname(datapaths[dataname][1]), dataname + '_Dataset.root')
+    def __init__(self, datapaths, variables, varnames = ()) :
+        self.datapaths = datapaths
+        self.variables = variables
+        self.varnames = varnames
+        self.make_getters()
 
-def get_dataset(dataname, variables, varnames, update = False) :
-    '''Get the RooDataSet of the given name. It's created/updated on demand. varnames is the 
-    set of variables to be included in the RooDataSet. They must correspond to those defined 
-    in the variables module. If the list of varnames changes or if update = True the 
-    RooDataSet will be recreated.'''
-    if not update :
-        fout = ROOT.TFile.Open(dataset_file_name(dataname))
-        if fout :
-            dataset = fout.Get(dataname)
-            cand = dataset.get(0)
-            if set(varnames) == set(cand.contentsString().split(',')) :
+    def get_data(self, name) :
+        '''Get the dataset of the given name.'''
+        try :
+            return make_chain(*self.datapaths[name])
+        except KeyError :
+            raise ValueError('Unknown data type: ' + repr(name))
+
+    def dataset_file_name(self, dataname) :
+        '''Get the name of the file containing the RooDataset corresponding to the given
+        dataset name.'''
+        return os.path.join(os.path.dirname(self.datapaths[dataname][1]), dataname + '_Dataset.root')
+
+    def get_dataset(self, dataname, varnames = None, update = False) :
+        '''Get the RooDataSet of the given name. It's created/updated on demand. varnames is the 
+        set of variables to be included in the RooDataSet. They must correspond to those defined 
+        in the variables module. If the list of varnames changes or if update = True the 
+        RooDataSet will be recreated.'''
+        if not varnames :
+            varnames = self.varnames
+        if not update :
+            fout = ROOT.TFile.Open(self.dataset_file_name(dataname))
+            if fout :
+                dataset = fout.Get(dataname)
+                cand = dataset.get(0)
+                if set(varnames) == set(cand.contentsString().split(',')) :
+                    fout.Close()
+                    return dataset
                 fout.Close()
-                return dataset
-            fout.Close()
 
-    print 'Making RooDataSet for', dataname
-    tree = get_data(dataname)
-    dataset = make_roodataset(dataname, dataname, tree,
-                              **dict((var, variables[var]) for var in varnames))
+        print 'Making RooDataSet for', dataname
+        tree = self.get_data(dataname)
+        dataset = make_roodataset(dataname, dataname, tree,
+                                  **dict((var, self.variables[var]) for var in varnames))
 
-    fname = dataset_file_name(dataname)
-    print 'Saving to', fname
-    fout = ROOT.TFile.Open(fname, 'recreate')
-    dataset.Write()
-    fout.Close()
-    return dataset
+        fname = self.dataset_file_name(dataname)
+        print 'Saving to', fname
+        fout = ROOT.TFile.Open(fname, 'recreate')
+        dataset.Write()
+        fout.Close()
+        return dataset
 
-def make_getters(namespace, datapaths) :
-    '''Define getter methods for every TTree dataset and corresponding RooDataSet.'''
-    for name in datapaths :
-        namespace[name] = eval('lambda : get_data({0!r})'.format(name))
-        namespace[name + '_Dataset'] = eval('lambda : get_dataset({0!r})'.format(name))
+    def make_getters(self) :
+        '''Define getter methods for every TTree dataset and corresponding RooDataSet.'''
+        for name in self.datapaths :
+            setattr(self, name, eval('lambda : get_data({0!r})'.format(name)))
+            setattr(self, name + '_Dataset', = eval('lambda : get_dataset({0!r})'.format(name)))
