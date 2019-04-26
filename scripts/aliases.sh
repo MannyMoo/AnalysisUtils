0563c2df624a04e11e7bbd24952a40ba943e54f5
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-04-26 15:06:54 +0100

    Fix aliases.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-RUNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../..
-alias run=${RUNDIR}/run
+RUNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../" && pwd )"
+echo "alias run=${RUNDIR}/run"
