66a9d191e9cb09a969cd50501304805e3e02af6d
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-10-07 16:07:53 +0100

    Use lb-run --ext pytools for run alias.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 RUNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../" && pwd )"
-echo "alias run=${RUNDIR}/run"
+PROJECT=$(basename $RUNDIR)
+# This is necessary to get pytools for ipython.
+echo "alias run=\"lb-run --ext pytools --user-area=${RUNDIR}/../ ${PROJECT/_//}\""
+# make install is needed to use lb-run.
+echo "alias build=\"cd ${RUNDIR};make && make install;cd -\""
+echo "alias confbuild=\"cd ${RUNDIR};make configure && make && make install;cd -\""
