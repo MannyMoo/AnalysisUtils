6b4a4fdcef27d74aaa3ff317a6de77d9d5293018
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-10-10 14:34:49 +0100

    Add --ignore option.

--- scripts/gen-xml-catalog.py
+++ scripts/gen-xml-catalog.py
@@ -8,7 +8,8 @@ parser.add_argument('lfnsfile', help = 'Options file containing the LFNs.')
 parser.add_argument('--xmlfile', default = None, help = 'Name of the xml file to save the catalog to.')
 parser.add_argument('--rootvar', default = None, help = 'Environment variable to use as the root of the xml file path')
 parser.add_argument('--nfiles', default = 0, help = 'Number of files to generate the catalog for.')
+parser.add_argument('--ignore', action = 'store_true', help = 'Whether to ignore inaccessible files.')
 args = parser.parse_args()
 
 gen_xml_catalog_from_file(args.lfnsfile, xmlfile = args.xmlfile, rootvar = args.rootvar,
-                          nfiles = int(args.nfiles))
+                          nfiles = int(args.nfiles), ignore = args.ignore)
--- scripts/get-bk-data.py
+++ scripts/get-bk-data.py
@@ -11,12 +11,14 @@ parser.add_argument('--xmlfile', default = None, help = 'Name of the xml file to
 parser.add_argument('--rootvar', default = None, help = 'Environment variable to use as the root of the xml file path')
 parser.add_argument('--nfiles', default = 0, help = 'Number of files to generate the catalog for.')
 parser.add_argument('--settings', '-s', action = 'store_true', help = 'Flag to save the data settings.')
+parser.add_argument('--ignore', action = 'store_true', help = 'Whether to ignore inaccessible files'
+                    ' when generating the xml catalog.')
 
 args = parser.parse_args()
 
 get_lfns_from_path(path = args.bkpath, outputfile = args.lfnsfile)
 if args.genxml :
     gen_xml_catalog_from_file(args.lfnsfile, xmlfile = args.xmlfile, rootvar = args.rootvar,
-                              nfiles = int(args.nfiles))
+                              nfiles = int(args.nfiles), ignore = args.ignore)
 if args.settings :
     get_data_settings(args.lfnsfile)
