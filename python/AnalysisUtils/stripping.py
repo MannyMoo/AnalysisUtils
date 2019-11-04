a687859cc5d3de32f4fbc36ab64e8fe003f6d320
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-11-04 11:59:38 +0000

    Add stripping module.

---
+++ python/AnalysisUtils/stripping.py
@@ -0,0 +1,6 @@
+'''Utilities for working with Stripping output.'''
+
+import os, sys
+
+sys.path.insert(0, os.path.join(os.environ['STRIPPINGSELECTIONSROOT'], 'tests', 'users'))
+from StrippingTuple import *
