742dde02d3535a3dbc08e5aaf131cc5aba03d1a1
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-11-21 10:44:04 +0000

    add option of different means to multi_gauss; add translate_scale_pdf

--- python/AnalysisUtils/fit.py
+++ python/AnalysisUtils/fit.py
@@ -3,9 +3,29 @@ from AnalysisUtils.treeutils import random_string, TreeFormula, TreeBranchAdder,
 
 def multi_gauss(workspace, name, variable, mean, sigmas, sigmamax, sigmaerror = 0.5,
                 fracerror = 0.05, meanwindow = 100., meanerror = 1.) :
-    if not isinstance(mean, ROOT.RooAbsReal) :
-        mean = workspace.roovar(name + '_mean', val = mean,
-                                xmin = mean - meanwindow, xmax = mean + meanwindow,
+    '''workspace: AnalysisUtils.Workspace.Workspace instance
+    name: name for the PDF
+    variable: x variable for the Gaussians
+    mean: mean of the Gaussians. Can be a list if they're to have different means.
+    sigmas: fractions and sigmas of the Gaussians. Should be either a single RooRealVar (single Gaussian)
+    or a list of (frac, sigma) pairs. The last element in the list is just the sigma. Fractions will be
+    used as in RSUM. Sigmas are just floats and should be in increasing order. The fit parameters will
+    be the differences between consecutive sigmas, which are forced to be positive.
+    sigmamax: upper limit for the first sigma and sigma differences.
+    sigmaerror: error on the sigmas.
+    fracerror: error on the fractions.
+    meanwindow: allowed range of the means around their central value.
+    meanerror: error on the means.
+    '''
+
+    def make_mean(val, i = None):
+        if isinstance(val, ROOT.RooAbsReal):
+            return val
+        _name = name + '_mean'
+        if None != i:
+            _name += '_' + str(i)
+        return workspace.roovar(_name, val = val, 
+                                xmin = val - meanwindow, xmax = val + meanwindow,
                                 error = meanerror)
     # Single Gaussian.
     if not isinstance(sigmas, (tuple, list)) :
@@ -14,12 +34,16 @@ def multi_gauss(workspace, name, variable, mean, sigmas, sigmamax, sigmaerror = 
                                                   xmin = 0., xmax = sigmamax,
                                                   error = sigmaerror))
 
+    if not isinstance(mean, (tuple, list)):
+        mean = [mean] * len(sigmas)
+    means = [make_mean(m) for m in mean]
+
     firstfrac, firstsigma = sigmas[0]
     firstfrac = workspace.roovar(name + '_frac_0', val = firstfrac, xmin = 0., xmax = 1.,
                                  error = fracerror)
     firstsigma = workspace.roovar(name + '_sigma_0', val = firstsigma, xmin = 0., xmax = sigmamax,
                                   error = sigmaerror)
-    firstpdf = workspace.factory('RooGaussian', name + '_0', variable, mean, firstsigma)
+    firstpdf = workspace.factory('RooGaussian', name + '_0', variable, means[0], firstsigma)
     pdfs = [(firstfrac, firstpdf)]
     prevsigma = firstsigma
     i = 0
@@ -29,7 +53,7 @@ def multi_gauss(workspace, name, variable, mean, sigmas, sigmamax, sigmaerror = 
         diff = workspace.roovar(name + '_sigma_diff_' + istr, val = sig - prevsigma.getVal(),
                                 xmin = 0., xmax = sigmamax, error = sigmaerror)
         sigma = workspace.factory('sum', name + '_sigma_' + istr, prevsigma, diff)
-        pdf = workspace.factory('RooGaussian', name + '_' + istr, variable, mean, sigma)
+        pdf = workspace.factory('RooGaussian', name + '_' + istr, variable, means[i+1], sigma)
         pdfs.append((frac, pdf))
         prevsigma = sigma
     sig = sigmas[-1]
@@ -37,7 +61,7 @@ def multi_gauss(workspace, name, variable, mean, sigmas, sigmamax, sigmaerror = 
     diff = workspace.roovar(name + '_sigma_diff_' + istr, val = sig - prevsigma.getVal(),
                             xmin = 0., xmax = sigmamax, error = sigmaerror)
     sigma = workspace.factory('sum', name + '_sigma_' + istr, prevsigma, diff)
-    lastpdf = workspace.factory('RooGaussian', name + '_' + istr, variable, mean, sigma)
+    lastpdf = workspace.factory('RooGaussian', name + '_' + istr, variable, means[-1], sigma)
     
     sumpdf = workspace.factory('RSUM', name,
                                *([frac.GetName() + '*' + pdf.GetName() for frac, pdf in pdfs] + [lastpdf.GetName()]))
@@ -51,6 +75,24 @@ def integral(pdf, variable, xmin, xmax) :
     args = ROOT.RooArgSet(variable)
     integrator = pdf.createIntegral(args, rangename)
     return integrator.getVal()
+
+def replace_variable(workspace, pdf, newname, oldvar, newvar):
+    '''Replace a variable in the PDF using RooCustomizer and import the new PDF into the workspace.'''
+    custom = ROOT.RooCustomizer(pdf, newname)
+    custom.replaceArg(oldvar, newvar)
+    newpdf = custom.build()
+    workspace.Import(newpdf)
+    return newpdf
+
+def translate_scale_pdf(workspace, pdf, newname, variable, translation, scale):
+    '''Make a copy of the given PDF with the given new name, translated and scaled in the given variable.
+    variable, translation and scale should be RooRealVars.'''
+
+    c0 = workspace.factory('expr', newname + '_c0', '"-{0}/{1}"'.format(translation.GetName(), scale.GetName()),
+                           translation, scale)
+    c1 = workspace.factory('expr', newname + '_c1', '"1./{0}"'.format(scale.GetName()), scale)
+    transvar = workspace.factory('RooLinearVar', newname + '_trans', variable, c1, c0)
+    return replace_variable(workspace, pdf, newname, variable, transvar)
 
 def sideband_subtraction_weight(pdf, variable, signalmin, signalmax, bkgmin, bkgmax,
                                 extrabkgints = (), extrasigints = ()) :
